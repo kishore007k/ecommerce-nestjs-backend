556cfa5b560c67c4ae6601008d6e2b9453361681
kishore <kishore.kumar@m8y.one>
2022-07-27 08:52:52 +0530

    Changed root log.

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return "Hi!, I am a NestJS app! :D";
   }
 }
